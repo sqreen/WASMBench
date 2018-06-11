54bface31da9ad292e1848fcb0b10e1457425ff0
vdeturckheim <vlad2t@hotmail.com>
2018-06-11 10:39:00 +0200

    add needed helpers

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,7 @@
 // The entry file of your WebAssembly module.
+import "allocator/arena";
+
+export { allocate_memory, free_memory }; // needed to manipulate strings and arrays from outside
 
 export function add(a: i32, b: i32): i32 {
   return a + b;
--- index.js
+++ index.js
@@ -17,4 +17,27 @@ asmModule.getStringArray = function (ptr) {
     return Array.from(U32.subarray(dataStart, dataStart + len), this.getString);
 };
 
+asmModule.newStringArray = function (arr) {
 
+    const dataLength = arr.length;
+    const pointers = arr.map((x) => this.newString(x));
+    const U32 = this.U32;
+
+    const ptr = this.allocate_memory(arr.length + 4);
+    const ptr0 = ptr >>> 2;
+
+    U32[ptr0 + 1] = dataLength;
+    const dataStart = ptr0 + 2;
+    U32[ptr0] = dataStart << 2;
+    U32[dataStart] = dataLength << 2;
+    U32[dataStart + 1] = 0;
+
+    for (let i = 0; i < dataStart; ++i) {
+        U32[dataStart + 2 + i] = pointers[i];
+    }
+    return ptr;
+};
+
+const ptr = asmModule.newStringArray(['a', 'b', 'j', 'aime', 'les', 'licornes']);
+
+console.log(asmModule.getStringArray(ptr));
