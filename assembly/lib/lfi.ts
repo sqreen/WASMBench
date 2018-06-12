92e3766591f63368434435f091f60bca7a037af8
vdeturckheim <vlad2t@hotmail.com>
2018-06-12 16:03:09 +0200

    reduce code

--- assembly/lib/lfi.ts
+++ assembly/lib/lfi.ts
@@ -11,13 +11,13 @@ const report = function (found: string, what: string): string[] {
 export function pre(filePath: string, params: string[]): string[] {
 
     for (let i = 0; i < params.length; ++i) {
-        const part = params[i];
+        var part = params[i];
         if (part.length > RELEVANT_INJECTED_SIZE && filePath.includes(part)) {
             if (part.startsWith('/') && filePath.startsWith('/') && filePath == part) {
                 return report(filePath, part);
             }
             if (filePath.endsWith(part)) {
-                let current = normalizePath(part);
+                var current = normalizePath(part);
                 // TODO: write memory efficien split
                 if (current.includes('../') || current.includes('../')) { // equivalent to splitted = current.split('/'); then (splitted.length > 1 && includes(splitted, '..')
                     return report(filePath, part);
